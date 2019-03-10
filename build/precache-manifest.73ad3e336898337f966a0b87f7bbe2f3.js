6cab5c06ae33c613994635fc1a3fcc75c4910e62
Andras Szesztai <andras@newzoo.com>
2019-03-10 20:11:08 +0100

    built

---
+++ build/precache-manifest.73ad3e336898337f966a0b87f7bbe2f3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "33cb7fd5e98cc2e5a0a0",
+    "url": "/economic-value-of-the-bicycle-industry/static/js/runtime~main.84c51d29.js"
+  },
+  {
+    "revision": "8ca9f2987cfd77964a98",
+    "url": "/economic-value-of-the-bicycle-industry/static/js/main.5a4444df.chunk.js"
+  },
+  {
+    "revision": "6e022240a40563d51f92",
+    "url": "/economic-value-of-the-bicycle-industry/static/js/2.7298012f.chunk.js"
+  },
+  {
+    "revision": "8ca9f2987cfd77964a98",
+    "url": "/economic-value-of-the-bicycle-industry/static/css/main.dd712659.chunk.css"
+  },
+  {
+    "revision": "0b11a7a372c5442640ba9d0664b9c7d4",
+    "url": "/economic-value-of-the-bicycle-industry/index.html"
+  }
+];
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.32f21d181fa2273ac9dcc29a39b47e57.js"
+  "/economic-value-of-the-bicycle-industry/precache-manifest.73ad3e336898337f966a0b87f7bbe2f3.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/economic-value-of-the-bicycle-industry/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
